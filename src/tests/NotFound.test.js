a2bf958d11c8f16776464f5250a0b6f655b9c7de
Helio Morais <heliogov20@gmail.com>
2023-05-02 18:36:30 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se a página contém um heading h2 com o texto Page requested not found;', () => {
+    renderWithRouter(<NotFound />);
+    const h2 = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+    const alt = 'Pikachu crying because the page requested was not found';
+    const img = screen.getByAltText(alt);
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
